e460e2a040c1df707f8440ef796fe012fbb92889
cosijoeza <cosi.melchor@gmail.com>
2024-08-12 23:16:45 -0600

    Add leasson 7 Likert

---
+++ estadistica-python/development/estadistica-descriptiva/main/likert-scale/leasson-7.py
@@ -0,0 +1,25 @@
+# LIKERT SCALE
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Genere information.
+np.random.seed(42)
+data = {
+    'School-A': np.random.choice([1,2,3,4,5],size=100,p=[0.05,0.15,0.5,0.20,0.10]),
+    'School-B': np.random.choice([1,2,3,4,5],size=100,p=[0.1,0.2,0.4,0.2,0.1]),
+    'School-C': np.random.choice([1,2,3,4,5],size=100,p=[0.15,0.35,0.30,0.15,0.05]),
+}
+
+# Set to dataframe.
+df = pd.DataFrame(data)
+
+# Set boxplot.
+sns.boxplot(data=df)
+
+# Set labels.
+plt.title('School Weather')
+plt.xlabel('School')
+plt.ylabel('Grade (Likert Scale)')
+plt.show()
