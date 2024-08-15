cb412322153f11c92c90c9ea42e351774cac9be8
cosijoeza <cosi.melchor@gmail.com>
2024-08-15 06:55:29 -0600

    Add likert scale

--- estadistica-python/README.md
+++ estadistica-python/README.md
@@ -1,13 +1,23 @@
 # ESTADISTICA CON PYTHON
 
-
 ## 1. Estadistica Descriptiva
- - 1.1 Medidas de tendencia central.
+
+- 1.1 Medidas de tendencia central
     -
-    - Media
-    - Mediana
-    - Moda
- - 1.2 Medidas de dispersión.
+
+  - Media
+  - Mediana
+  - Moda
+
+- 1.2 Medidas de dispersión
     -
-    - Rango
- - 1.3 Representación gráfica con python.
+
+  - Rango
+  - Rango Intercuartilico
+  - Desviación Estandar
+  - Varianza
+
+- 1.3 Representación gráfica con python
+   -
+
+  - Escala de Likert
---
+++ estadistica-python/development/estadistica-descriptiva/graphics/survey-likert.py
@@ -0,0 +1,42 @@
+#
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Answers simulation.
+np.random.seed(42)
+
+satisfaction_data = np.random.choice(
+    a=[
+        "1.-Muy Insatisfecho",
+        "2.-Insatisfecho",
+        "3.-Neutral",
+        "4.-Satisfecho",
+        "5.-Muy Satisfecho",
+    ],
+    size=300,
+    p=[0.05, 0.15, 0.20, 0.40, 0.20],
+)
+
+# Create dataframe.
+df = pd.DataFrame(satisfaction_data, columns=["Satisfaccion"])
+
+# Create frecuency tables.
+frecuency_table = df["Satisfaccion"].value_counts().sort_index()
+
+print(frecuency_table)
+
+# Graphics configuration.
+sns.set_theme(style="whitegrid")
+
+print("Index: ",frecuency_table.index)
+print("Values: ",frecuency_table.values)
+
+# Create bar graph.
+plt.figure(figsize=(10,6))
+sns.barplot(x = frecuency_table.index,y=frecuency_table.values,palette='Blues_d')
+plt.title('Frecuencia de satisfaccion de servicios de biblioteca')
+plt.xlabel('Nivel de satisfaccion')
+plt.ylabel('Frecuencia')
+plt.show()
