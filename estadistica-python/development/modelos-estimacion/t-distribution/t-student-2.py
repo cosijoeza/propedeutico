dd479dd36c3fc5e121da5e7a5c746f289da1e002
cosijoeza <cosi.melchor@gmail.com>
2024-09-05 20:54:31 -0600

    update t distribution

--- estadistica-python/development/modelos-estimacion/t-distribution/t-student-2.py
+++ estadistica-python/development/modelos-estimacion/t-distribution/t-student-2.py
@@ -10,23 +10,30 @@ calificaciones = [86, 90, 92, 88, 87, 84, 91, 89, 86, 87]
 
 # Nivel de confianza.
 confianza = 0.95
-
+# Nivel de significancia.
+significancia = 1 + confianza
 # Grados de libertad.
-df = len(calificaciones) - 1
+gradosLibertad = len(calificaciones) - 1
 
 # Media y desviacion estandar de la muestra
-media_muestra = np.mean(calificaciones)
+mediaMuestra = np.mean(calificaciones)
 # ddof= 1 par acalcular la desviacion estandar muestral.
-desviacion_muestra = np.std(calificaciones, ddof=1)
-
+desviacionMuestra = np.std(calificaciones, ddof=1)
 # Valor critico de t.
-valorCritico = stats.t.ppf((1 + confianza))
+valorCritico = stats.t.ppf(significancia / 2, gradosLibertad)
+# Error estandar.
+errorEstandar = desviacionMuestra / np.sqrt(len(calificaciones))
 
-# # Error estandar.
-errorEstandar = desviacion_muestra / np.sqrt(len(calificaciones))
+print(
+    " Media de la muestra:{:.6f}\n Desviacion de la muestra:{:.6f}\n Valor critico: {:.6f}".format(
+        mediaMuestra, desviacionMuestra, valorCritico
+    )
+)
 
 # Limites del intervalo de confianza.
-intervalo_inferior = media_muestra - valorCritico * errorEstandar
-intervalo_superior = media_muestra + valorCritico * errorEstandar
+intervalo_inferior = mediaMuestra - valorCritico * errorEstandar
+intervalo_superior = mediaMuestra + valorCritico * errorEstandar
 
-print("Intervalo de confianza al {confianza*100}%: ({intervalo_inferior:2f})")
+print(
+    f"Intervalo de confianza al {confianza*100}%: ({intervalo_inferior:2f}, {intervalo_superior:2f})"
+)
